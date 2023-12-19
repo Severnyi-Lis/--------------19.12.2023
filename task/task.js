ebeb0689d482df2321e4b132542d350407eca1b3
Severnyi-Lis <xnestro@mail.ru>
2023-12-19 20:56:36 +0300

    Добавил вычесление возраста

--- task/task.js
+++ task/task.js
@@ -1,1 +1,8 @@
 console.log('Скрипт работает');
+console.log(document.forms.my_body.mass.value);
+function calc_age(year_of_birth) {
+    let dt = new Date();
+    var result = dt.getFullYear() - year_of_birth;
+    age.innerHTML = result;
+	return result;
+}
